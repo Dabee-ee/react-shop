ba804152bb7f7cdae2f03a6183b241188b510c63
Dabee-ee <movie2529@naver.com>
2022-08-17 17:25:12 +0900

    first commit

---
+++ src/pages/products/[id].tsx
@@ -0,0 +1,3 @@
+const ProductDetail = () => <div>상세 페이지 입니다.</div>;
+
+export default ProductDetail;
---
+++ src/pages/products/index.tsx
@@ -0,0 +1,3 @@
+const ProductList = () => <div>상품 목록 페이지 입니다.</div>;
+
+export default ProductList;
